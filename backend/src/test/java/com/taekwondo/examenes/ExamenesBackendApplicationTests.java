76ee20210587dcff30e464524d57917244174115
ABEL RODRIGUEZ GUTIERREZ <arg331@inlumine.ual.es>
2026-04-30 18:55:39 +0200

    feat: Base del proyecto

---
+++ backend/src/test/java/com/taekwondo/examenes/ExamenesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taekwondo.examenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
